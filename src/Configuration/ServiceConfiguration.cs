f33bd22b7f4c0e0a76409a0cf9f126ba8ab58655
Daniel Yordanov <dakataebg@students.softuni.bg>
2025-03-23 01:07:21 +0200

    feat: add DI & register configuration

---
+++ src/Configuration/ServiceConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using FileMonitoringApp.Services.Monitoring;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FileMonitoringApp.Configuration
+{
+    internal static class ServiceConfiguration
+    {
+        public static IServiceCollection Configure(this IServiceCollection services)
+        {
+            return services
+                .AddConfiguration();
+        }
+
+        private static IServiceCollection AddConfiguration(this IServiceCollection services)
+        {
+            return services.AddSingleton<IConfiguration>(provider =>
+                 new ConfigurationBuilder()
+                     .AddJsonFile("appsettings.json")
+                     .Build()
+             );
+        }
+
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,22 @@
+﻿using FileMonitoringApp.Configuration;
+using FileMonitoringApp.Services.Monitoring;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FileMonitoringApp
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var serviceProvider = CreateConfiguredServiceProvider();
+
+        }
+
+        private static IServiceProvider CreateConfiguredServiceProvider()
+        {
+            return new ServiceCollection()
+                .Configure()
+                .BuildServiceProvider();
+        }
+    }
+}
